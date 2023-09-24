8c7a2d0fc4ac651323775a5eee317063be20463d
juan cordoba <juancm1106.2@gmail.com>
2023-09-23 20:04:53 -0500

    Cree la pagina principal

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card>
+    <v-tabs style="background-color: black; color: azure;">
+      <nuxt-link style="color: aliceblue;" v-for="item in items" :key="item.tab" :to="item.route">
+        <v-tab>{{ item.tab }}</v-tab>
+      </nuxt-link>
+    </v-tabs>
+    <router-view></router-view>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      tab: null,
+      items: [
+        { tab: "Inicio", route: "/" },
+        { tab: "Personajes", route: "/heroes" },
+      ],
+    };
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-sheet class="d-flex align-center justify-center flex-wrap"
+    style="background-image: url('https://getwallpapers.com/wallpaper/full/a/b/8/244583.jpg');
+    height: 100vh">
+  
+  <div style="text-align: center; 
+  background-color: rgba(0, 0, 0, 0.375); 
+  border-radius: 20px; 
+  max-width: 80%; 
+  padding-top: 5%; 
+    color: white;">
+  <center>
+    <div style="padding-bottom: 5%;">
+        <div style="background-color: rgba(0, 0, 0, 0.544); border-radius: 20px; max-width: 50%; padding: 3%; border-color: red; border-style: groove;">
+            <h1 style="color: white; background-color: red; border-radius: 20px; max-width: 40%; font-family: Verdana, Geneva, Tahoma, sans-serif">Bienvenid@!</h1>
+        </div>
+    </div>
+    </center>
+        <center>
+            <div style="background-color: rgba(111, 0, 0, 0.766); border-radius: 20px; max-width: 80%;">
+                <p style="padding: 3%; color: white; font-size: medium;font-family: Verdana, Geneva, Tahoma, sans-serif ">
+                    Bienvenid@ a mi pagina web, donde podrás encontrar un catálogo de personajes de Marvel.
+                    Aquí podrás conocer más sobre tus héroes y diversa informacion sobre ellos asi como imagenes.
+                </p>
+                <p style="padding: 3%; font-size: medium;font-family: Verdana, Geneva, Tahoma, sans-serif">
+                    Espero que disfrutes de esta página web y que te diviertas explorando el universo Marvel.
+                </p>
+            </div>
+        </center>
+    <center>
+        <div style="max-width: 80%; padding-top: 5%; padding-bottom: 5%;">
+            <p style="padding: 3%; background-color: rgba(111, 0, 0, 0.766); border-radius: 20px; font-family: Verdana, Geneva, Tahoma, sans-serif">Navega por el menú superior y elige el personaje que más te interese.</p> 
+        </div>
+    </center>
+  </div>
+    <!--<div style="background-color: white;">
+      <nuxt-link to="/characters" style="font-weight: 500;
+        font-family: Didot, serif;
+        font-size: xx-large;
+        text-align: center;">
+  
+        character catalog 
+      </nuxt-link>
+    </div> -->
+    </v-sheet>
+  
+</template>
