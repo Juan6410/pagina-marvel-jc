02b6c0cf79958777be8403817fb507e2aa7dd40d
juan cordoba <juancm1106.2@gmail.com>
2023-09-23 21:25:30 -0500

    Se agrego el logo a la barra superior

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,9 @@
       <nuxt-link style="color: aliceblue;" v-for="item in items" :key="item.tab" :to="item.route">
         <v-tab>{{ item.tab }}</v-tab>
       </nuxt-link>
+      <div style="align-self: flex-end;">
+        <img src="https://th.bing.com/th/id/R.2ec569bffe9defbe2930ed8aa8135388?rik=vcx0LtGUqhxpZQ&pid=ImgRaw&r=0" height="30px" width="70px">
+      </div>
     </v-tabs>
     <router-view></router-view>
   </v-card>
